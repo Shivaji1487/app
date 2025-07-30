d171098164cf858d4903e89ca763e78ff95c24ff
Shivaji1487 <80825214+Shivaji1487@users.noreply.github.com>
2025-07-30 14:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-print("Testing CI/CD pipeline for jenkins for auto fetch the deatails after changing in git code")
+print"Testing CI/CD pipeline for jenkins for auto fetch the deatails after changing in git code")
